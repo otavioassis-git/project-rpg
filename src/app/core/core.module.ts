2fd6151f7c07bb3bc908841e816f01d9fd9afbd4
otavioassis-git <otavioassis2013@gmail.com>
2023-05-07 20:04:51 -0300

    adding primeng

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, ButtonModule],
+  exports: [ButtonModule],
 })
-export class CoreModule { }
+export class CoreModule {}
--- src/app/modules/main-window/main-window.module.ts
+++ src/app/modules/main-window/main-window.module.ts
@@ -1,17 +1,12 @@
+import { CoreModule } from './../../core/core.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainWindowRoutingModule } from './main-window-routing.module';
 import { MainWindowComponent } from './pages/main-window/main-window.component';
 
-
 @NgModule({
-  declarations: [
-    MainWindowComponent
-  ],
-  imports: [
-    CommonModule,
-    MainWindowRoutingModule
-  ]
+  declarations: [MainWindowComponent],
+  imports: [CommonModule, MainWindowRoutingModule, CoreModule],
 })
-export class MainWindowModule { }
+export class MainWindowModule {}
