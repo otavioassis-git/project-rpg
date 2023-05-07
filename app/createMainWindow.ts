d72e9f3af94d01c7b3b722e90abf5364a13140aa
otavioassis-git <otavioassis2013@gmail.com>
2023-05-07 19:37:58 -0300

    initial electron configuration

---
+++ app/createMainWindow.ts
@@ -0,0 +1,58 @@
+import { BrowserWindow, protocol, screen } from 'electron';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as url from 'url';
+
+const args = process.argv.slice(1),
+  serve = args.some((val) => val === '--serve');
+
+export function createMainWindow(): BrowserWindow {
+  const size = screen.getPrimaryDisplay().workAreaSize;
+
+  // Create the browser window.
+  let win = new BrowserWindow({
+    x: 0,
+    y: 0,
+    width: size.width,
+    height: size.height,
+    webPreferences: {
+      // devTools: false,
+      nodeIntegration: true,
+      allowRunningInsecureContent: serve,
+      contextIsolation: false, // false if you want to run e2e test with Spectron
+    },
+  });
+
+  if (serve) {
+    const debug = require('electron-debug');
+    debug();
+
+    require('electron-reloader')(module);
+    win.loadURL('http://localhost:4200');
+  } else {
+    // Path when running electron executable
+    let pathIndex = './index.html';
+
+    if (fs.existsSync(path.join(__dirname, '../dist/index.html'))) {
+      // Path when running electron in local folder
+      pathIndex = '../dist/index.html';
+    }
+
+    const ref = url.format({
+      pathname: path.join(__dirname, pathIndex),
+      protocol: 'file:',
+      slashes: true,
+    });
+    win.loadURL(ref);
+  }
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store window
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+
+  return win;
+}
--- app/main.ts
+++ app/main.ts
@@ -1,56 +1,10 @@
-import {app, BrowserWindow, screen} from 'electron';
-import * as path from 'path';
-import * as fs from 'fs';
+import { app, BrowserWindow, screen } from 'electron';
+import { createMainWindow } from './createMainWindow';
 
-let win: BrowserWindow = null;
-const args = process.argv.slice(1),
-  serve = args.some(val => val === '--serve');
+let mainWin: BrowserWindow = null;
 
-function createWindow(): BrowserWindow {
-
-  const size = screen.getPrimaryDisplay().workAreaSize;
-
-  // Create the browser window.
-  win = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: size.width,
-    height: size.height,
-    webPreferences: {
-      nodeIntegration: true,
-      allowRunningInsecureContent: (serve),
-      contextIsolation: false,  // false if you want to run e2e test with Spectron
-    },
-  });
-
-  if (serve) {
-    const debug = require('electron-debug');
-    debug();
-
-    require('electron-reloader')(module);
-    win.loadURL('http://localhost:4200');
-  } else {
-    // Path when running electron executable
-    let pathIndex = './index.html';
-
-    if (fs.existsSync(path.join(__dirname, '../dist/index.html'))) {
-       // Path when running electron in local folder
-      pathIndex = '../dist/index.html';
-    }
-
-    const url = new URL(path.join('file:', __dirname, pathIndex));
-    win.loadURL(url.href);
-  }
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store window
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
-
-  return win;
+function App() {
+  mainWin = createMainWindow();
 }
 
 try {
@@ -58,7 +12,7 @@ try {
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
   // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
-  app.on('ready', () => setTimeout(createWindow, 400));
+  app.on('ready', () => setTimeout(App, 400));
 
   // Quit when all windows are closed.
   app.on('window-all-closed', () => {
@@ -72,11 +26,10 @@ try {
   app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow();
+    if (mainWin === null) {
+      createMainWindow();
     }
   });
-
 } catch (e) {
   // Catch Error
   // throw e;
