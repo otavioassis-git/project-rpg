2198c8bea2d411bbdb4f534c4d987fcd9b1d7aec
otavioassis-git <otavioassis2013@gmail.com>
2023-05-08 15:16:14 -0300

    wip

--- src/app/modules/map-hider/pages/map-hider/map-hider.component.ts
+++ src/app/modules/map-hider/pages/map-hider/map-hider.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-map-hider',
   templateUrl: './map-hider.component.html',
-  styleUrls: ['./map-hider.component.scss']
+  styleUrls: ['./map-hider.component.scss'],
 })
 export class MapHiderComponent {
+  image;
 
+  constructor() {}
+
+  loadImage(event) {
+    const file = (event.target as HTMLInputElement).files[0];
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.image = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
 }
