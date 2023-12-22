fd095bb1095822dd4859ce903405b51a145fdc9f
otavioassis-git <otavioassis2013@gmail.com>
2023-12-22 17:54:31 -0300

    wip

--- app/main.ts
+++ app/main.ts
@@ -44,6 +44,13 @@ function App() {
   }
 
   mainWin = createMainWindow();
+  if (showDevTools)
+    contextMenu({
+      window: mainWin,
+      showCopyImage: false,
+      showInspectElement: true,
+      showSelectAll: false,
+    });
   previousBounds = mainWin.getBounds();
   loadEvents();
   autoUpdater.checkForUpdates();
@@ -94,7 +101,7 @@ try {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWin === null) {
-      createMainWindow();
+      mainWin = createMainWindow();
     }
   });
 } catch (e) {
