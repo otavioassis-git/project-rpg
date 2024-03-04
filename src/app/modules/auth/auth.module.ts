2891ec7d27db7268d0697df588d9dc6bdbcfdf0b
otavioassis-git <otavioassis2013@gmail.com>
2024-03-04 16:33:37 -0300

    creating login and signin screens

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { SigninComponent } from './pages/signin/signin.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [LoginComponent, SigninComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, InputTextModule, ButtonModule],
 })
 export class AuthModule {}
