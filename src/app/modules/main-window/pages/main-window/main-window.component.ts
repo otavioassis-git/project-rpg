18aa12a43f0d20cee1e4bf7472c528d5e78ff1ec
otavioassis-git <otavioassis2013@gmail.com>
2024-03-06 20:58:12 -0300

    refactoring tutorial setting code

--- src/app/modules/main-window/pages/main-window/main-window.component.ts
+++ src/app/modules/main-window/pages/main-window/main-window.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
+import { Tutorials } from '../../../../core/services/tutorial.service';
+import { resolve } from 'path';
 
 @Component({
   selector: 'app-main-window',
@@ -8,11 +12,51 @@ import { Component, OnInit } from '@angular/core';
 export class MainWindowComponent implements OnInit {
   username: string = '';
 
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
     if (localStorage.getItem('user')) {
       this.username = JSON.parse(localStorage.getItem('user')).username;
     }
+
+    this.loadSettings();
+  }
+
+  loadSettings() {
+    this.loadTutorials();
+  }
+
+  loadTutorials() {
+    this.httpClient
+      .get('assets/isServe.json')
+      .pipe(take(1))
+      .subscribe((value: any) => {
+        if (value.serve) {
+          this.httpClient
+            .get('assets/tutorials.json')
+            .pipe(take(1))
+            .subscribe((value: Tutorials) => {
+              localStorage.setItem('tutorials', JSON.stringify(value));
+            });
+        } else {
+          try {
+            let value: Tutorials = JSON.parse(
+              window
+                .require('fs')
+                .readFileSync(
+                  resolve(
+                    __dirname,
+                    '../',
+                    '../',
+                    '../',
+                    'Project-RPG-common',
+                    'tutorials.json'
+                  )
+                )
+            );
+            localStorage.setItem('tutorials', JSON.stringify(value));
+          } catch (error) {}
+        }
+      });
   }
 }
