e028852089daabc35e2ad4a1248b8060fc9faa52
otavioassis-git <otavioassis2013@gmail.com>
2024-03-06 22:23:12 -0300

    hidding change env button

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import packageInfo from '../../../../package.json';
 import { DialogService } from 'primeng/dynamicdialog';
 import { EnvChangerComponent } from './components/env-changer/env-changer.component';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-auth-layout',
@@ -11,12 +12,26 @@ import { EnvChangerComponent } from './components/env-changer/env-changer.compon
 })
 export class AuthLayoutComponent {
   lVersion = packageInfo.version;
+  clickCount = 0;
+  showChangeEnv = false;
 
   constructor(private dialog: DialogService) {}
 
+  clickCountHandler() {
+    this.clickCount++;
+    if (this.clickCount == 5) {
+      this.showChangeEnv = true;
+    }
+  }
+
   openEnvChanger() {
-    this.dialog.open(EnvChangerComponent, {
+    const ref = this.dialog.open(EnvChangerComponent, {
       header: 'Change backend environment',
+    });
+
+    ref.onClose.pipe(take(2)).subscribe(() => {
+      this.clickCount = 0;
+      this.showChangeEnv = false;
     });
   }
 }
